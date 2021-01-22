724b18df3603450ec2e2a1c16e25d9227fdd43aa
kodtrollet <h20tebe@axevalla.fhsk.se>
2021-01-22 09:46:46 +0100

    teddy tutorial

---
+++ teddy/training/hud.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+signal start_game
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func show_message(text):
+	$message.text = text
+	$message.show()
+	$messagetimer.start()
+
+func show_game_over():
+	show_message("game over")
+	
+	yield($messagetimer, "timeout")
+	
+	$message.text = "dodge the\ncreeps!"
+	$message.show()
+	
+	yield(get_tree().create_timer(1), "timeout")
+	$startbutton.show()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
