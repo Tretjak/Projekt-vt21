c23565a48abdf69e69f60ee860d7938e6996a2ce
kodtrollet <h20tebe@axevalla.fhsk.se>
2021-02-22 11:04:53 +0100

    krasha

--- Projekt vt21/StaticBody2D.gd
+++ Projekt vt21/StaticBody2D.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 var block = true
+var knapp = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,9 +9,21 @@ var block = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("ui_e") and knapp:
+		queue_free().CollisionShape2D
+
+
+func _on_Area2D_body_entered(body):
+	if body.get_class() == "KinematicBody2D":
+		knapp = true
+		print("öppna")
+
+
+
+func _on_Area2D_body_exited(body):
+	knapp = false
