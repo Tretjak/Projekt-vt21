3e23f37bde2b53802ac5a45d0fb4d8189490fcf4
samuelmanolakas <samimanolakas@gmail.com>
2021-02-24 13:01:32 +0200

    fix

--- Projekt vt21/src/Enemy.gd
+++ Projekt vt21/src/Enemy.gd
@@ -1,15 +1,22 @@
 extends KinematicBody2D
 
-var motion = Vector2()
 
+onready var nav_2d: Navigation2D = get_node("../../Navigation2D")
+
+var goal = Vector2()
+var motion = Vector2()
+var path = []
 var state = 0
 #0 = stop, 1 = right, 2 = left, 3 = up, 4 = down
 
 func _physics_process(delta):
+	_set_target(goal)
+	if Input.is_action_pressed('ui_right'):
+		goal = get_node("../../player").global_position
+	move_and_slide(goal)
 	
 	
-	
-	if state ==0:
+	"""if state ==0:
 		motion.x = 0
 		motion.y = 0
 	elif state == 1:
@@ -24,11 +31,19 @@ func _physics_process(delta):
 	elif state == 4:
 		motion.x = 0
 		motion.y = 100
+		"""
 	
-	move_and_slide(motion, Vector2(0,-1))
+	
 
 
 func _on_Timer_timeout():
 	state = floor(rand_range(0,5))
 	print(state)
 	pass # Replace with function body.
+	
+	
+func _set_target(goal):
+	var new_path : = nav_2d.get_simple_path(self.golab_position, goal)
+	print("what is my path?")
+	print(new_path)
+	path = new_path
--- Projekt vt21/src/KinematicBody2D.gd
+++ Projekt vt21/src/KinematicBody2D.gd
@@ -8,11 +8,3 @@ onready var monster = get_node("res://Enemy2")
 onready var goal = get_node("KinematicBody2D")
 # var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _process(delta):
-	_set_traget(goal)
-
-
-func _set_target(target):
-	var new_path : = nav_2d.get_simple_path( )
