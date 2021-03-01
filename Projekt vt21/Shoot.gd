b6a546d671d8d1b90451a92677011da8da677454
Tobbelidzen <48048136+Tobbelidzen@users.noreply.github.com>
2021-03-01 11:20:07 +0100

    Rename Script
    Rename Node2D.gd to Shoot.gd

--- Projekt vt21/Shoot.gd
+++ Projekt vt21/Shoot.gd
@@ -1,16 +1,15 @@
 extends Area2D
 
+const speed = 5
+var target
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity =Vector2()
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	target = get_global_mouse_position()
+	velocity = -(position - target).normalized()
+	pass
+	
+func _process(delta):
+	position += velocity * speed
+	
