3c9d26b9607534a2eae69111cc587efc4603d578
samuelmanolakas <samimanolakas@gmail.com>
2021-03-05 12:59:29 +0200

    Cleanup

--- Projekt vt21/Enemy/Enemy.gd
+++ Projekt vt21/Enemy/Enemy.gd
@@ -8,7 +8,7 @@ var goal = Vector2()
 var motion = Vector2()
 var path = []
 var state = 0
-var hunt = 1
+var hunt = 0
 #0 = stop, 1 = right, 2 = left, 3 = up, 4 = down
 
 func _physics_process(delta):
@@ -17,30 +17,31 @@ func _physics_process(delta):
 		if Input.is_action_pressed('ui_right'):
 			goal = get_node("../../player").global_position - global_position
 		move_and_slide(goal * MOVE_SPEED * delta)
-	
-"""if state ==0:
-		motion.x = 0
-		motion.y = 0
-	elif state == 1:
-		motion.x = 100
-		motion.y = 0
-	elif state == 2:
-		motion.x = -100
-		motion.y = 0
-	elif state == 3:
-		motion.x = 0
-		motion.y = -100
-	elif state == 4:
-		motion.x = 0
-		motion.y = 100
-		"""
+	elif hunt == 0:
+		if state ==0:
+			motion.x = 0
+			motion.y = 0
+		elif state == 1:
+			motion.x = 100
+			motion.y = 0
+		elif state == 2:
+			motion.x = -100
+			motion.y = 0
+		elif state == 3:
+			motion.x = 0
+			motion.y = -100
+		elif state == 4:
+			motion.x = 0
+			motion.y = 100
+			
 
 
-"""func _on_Timer_timeout():
-	state = floor(rand_range(0,5))
-	print(state)
-	pass # Replace with function body.
-	"""
+func _on_Timer_timeout():
+	if hunt == 0:
+		state = floor(rand_range(0,5))
+		print(state)
+		pass # Replace with function body.
+		
 	
 	
 func _set_target(goal):
