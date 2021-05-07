b5386463393de9117e6e9f601c7db9f6925374f1
kodtrollet <h20tebe@axevalla.fhsk.se>
2021-05-07 09:57:49 +0200

    spelar anim och startmeny

---
+++ Projekt vt21/fortsätt.gd
@@ -0,0 +1,20 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var button = Button.new()
+	button.text = "ladda spel"
+	button.connect("pressed", self, "_button_pressed")
+	add_child(button)
+
+func _button_pressed():
+	print("Kommer snart till en dator långt borta från dig")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
